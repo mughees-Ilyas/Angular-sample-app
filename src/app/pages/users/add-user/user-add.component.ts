e69f1bf4dcc5055a30f92ca892d8ea388cd67627
mughees <mugheesilyas@live.com>
2019-11-18 16:18:01 +0100

    add functionality to let new user login

--- src/app/pages/users/add-user/user-add.component.ts
+++ src/app/pages/users/add-user/user-add.component.ts
@@ -62,7 +62,7 @@ export class UserAddComponent {
       this.submitted = true;
       let dataObj = this.f.DateOfBirth.value;
     let dateOfbirth = dataObj.day + "-" + dataObj.month + "-"+ dataObj.year;
-    let data =   { id: Date.now(), username: this.f.firstName.value, password: 'test', firstName: this.f.firstName.value, lastName: this.f.lastName.value, address:this.f.address.value, dob:dateOfbirth };
+    let data =   { id: Date.now(), username: this.f.firstName.value, password: this.f.firstName.value, firstName: this.f.firstName.value, lastName: this.f.lastName.value, address:this.f.address.value, dob:dateOfbirth };
       this.userService.addUser(data)
       .pipe(first())
       .subscribe(
